aa3644877bf1b1e1c958d119df097c3fcdd77ccd
Carlos <csilva@mredis.com>
2021-12-10 12:30:52 +0000

    update  comunication public

--- resources/views/site_v2/pages/communication-public.blade.php
+++ resources/views/site_v2/pages/communication-public.blade.php
@@ -1,73 +1,65 @@
- @extends('site_v2/layouts/default')
+@extends('site_v2/layouts/default')
 
 @section('content')
 
- <div class="container mt-5 mb-5">
-     
-     <div>
-         <h1 class="text-black mb-5">Area de comunicação de Imagens </h1>
-     </div>
-<table class="table table-striped mt-5">
-  <thead class="table-dark">
-    <tr>
-         <th scope="col">#</th>
-         <th  scope="col">{{ trans('backoffice.communiFile') }}</th>
-        <th  scope="col">{{ trans('backoffice.communNome') }}</th>
-        <th  scope="col">{{ trans('backoffice.comunDesc') }}</th>
-        <th  scope="col">{{ trans('backoffice.communUpdate') }}</th>
-        <th  scope="col">{{ trans('backoffice.communTipo') }}</th>
-         <th class="text-center"  scope="col">{{ trans('backoffice.commuAction') }}</th>
-    </tr>
-  </thead>
-  <tbody>
-        @foreach($array as $val)
-       <tr id="linha_{{ $val['id'] }}">
-    
+<div class="container">
+
+  <table class="table table-striped" style="margin-top:150px;">
+    <thead class="table-dark mt-5">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">{{ trans('backoffice.communiFile') }}</th>
+        <th scope="col">{{ trans('backoffice.communNome') }}</th>
+        <th scope="col">{{ trans('backoffice.comunDesc') }}</th>
+        <th scope="col">{{ trans('backoffice.communUpdate') }}</th>
+        <th scope="col">{{ trans('backoffice.communTipo') }}</th>
+        <th class="text-center" scope="col">{{ trans('backoffice.commuAction') }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($array as $val)
+      <tr id="linha_{{ $val['id'] }}">
+
         <td>{!! $val['id'] !!}</td>
-         <td>{!! $val['ficheiro'] !!}</td>
-         <td>{!! $val['nome'] !!}</td>
-         <td>{!! $val['descr'] !!}</td>
-         <td>{!! $val['update'] !!}</td>
-          <td>{!! $val['tipo'] !!}</td>
-          <td class="text-center">
-        {{-- download   --}}
-        <a  class="table-opcao" href="{{ route('downloadPage',['id'=>$val['id']]) }}">
+        <td>{!! $val['ficheiro'] !!}</td>
+        <td>{!! $val['nome'] !!}</td>
+        <td>{!! $val['descr'] !!}</td>
+        <td>{!! $val['update'] !!}</td>
+        <td>{!! $val['tipo'] !!}</td>
+        <td class="text-center">
+          {{-- download   --}}
+          <a class="table-opcao" href="{{ route('downloadPage',['id'=>$val['id']]) }}">
             <span class="table-opcao">
-            <i class="fas fa-file-download"></i>&nbsp;{{trans('backoffice.commuDownload')}}
+              <i class="fas fa-file-download"></i>&nbsp;{{trans('backoffice.commuDownload')}}
             </span>
-        </a>&ensp;
-         {{-- share link  --}}
-            <input type="hidden" id="id_copy-{{$val['id']}}"  value="{{ route('downloadPage',['id'=>$val['id']]) }}">
-            <span class="table-opcao"  id="btnShareLink"  onclick="copyLink_v2({{$val['id']}})"  style="cursor:pointer;">
-                   <i class="fas fa-cloud-download-alt"></i>&nbsp;{{trans('backoffice.commuShare')}}
-            </span>
-          </td>
-    </tr>
-     @endforeach
-  </tbody>
-</table>
+          </a>&ensp;
+          {{-- share link  --}}
+          <input type="hidden" id="id_copy-{{$val['id']}}" value="{{ route('downloadPage',['id'=>$val['id']]) }}">
+          <span class="table-opcao" id="btnShareLink" onclick="copyLink_v2({{$val['id']}})" style="cursor:pointer;">
+            <i class="fas fa-cloud-download-alt"></i>&nbsp;{{trans('backoffice.commuShare')}}
+          </span>
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
 
 @stop
 @section('css')
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/css/swiper.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/css/swiper.min.css">
 @stop
 
 @section('javascript')
 <script src="{{ asset('backoffice/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-	<script>
-		$('.header-span').css('color','#333');
+<script>
+  $('.header-span').css('color','#333');
 		$('.header-submenu').css('background-image','linear-gradient(180deg, #eee, #fbfbfb)');
 		$('.header-submenu-angle').css('border-bottom','15px solid rgba(0,0,0,0.06)');
 		$('.header-submenu a').css('color','#333');
 		$('.header').css('background-color','#fff');
 		$('.header-xs').css('background-color','#fff');
-		/*$('.table-img-circle').css('height','50px')
-		$('.table-img-circle').css('width':'auto')
-		$('.table-img-circle').css('border-radius','50%')*/
 
-	
-	
 	 function copyLink_v1(id){
       
      /* Get the text field */
@@ -95,6 +87,6 @@
         document.body.removeChild(sampleTextarea);
    }
    
-   	</script>
-    
+</script>
+
 @stop
